850c9bbe89daa47b55854592dd2def6ba4edb6f2
Adam Sjöström <as223js@student.lnu.se>
2023-08-31 20:01:14 +0200

    add styling and more funtionality to textToSpeech.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import TextToSpeech from "../components/textToSpeech/textToSpeech"
+import styles from "../styles/mainPage.module.css"
 
 export default function Home() {
 
   return (
     <main>
-      <h1>Labb 1</h1>
-      <TextToSpeech />
+      <h1 className={styles.headLine}>Labb 1</h1>
+      <div className={styles.inputField}>
+        <TextToSpeech />
+      </div>
     </main>
   )
 }
--- components/textToSpeech/textToSpeech.js
+++ components/textToSpeech/textToSpeech.js
@@ -1,12 +1,66 @@
 'use client'
 
 import { useState } from 'react'
+import styles from '../../styles/inputStyle.module.css'
 
 const TextToSpeech = () => {
   const [userInput, setUserInput] = useState("")
 
   const handleSpeak = () => {
-      const message = `Hello ${userInput}`
+      let message = ""
+      switch (userInput[0].toLowerCase()) {
+        case 'a':
+        case 'b':
+        case 'j':
+          message = `Live long and prosper ${userInput}`
+          break
+        case 'c': 
+        case 'o':
+        case 'p':
+          message = `Greetings ${userInput}`
+          break
+        case 'd':
+        case 'g':
+        case 't':
+          message = `Hello there ${userInput}`
+          break
+        case 'f':
+        case 'e':
+        case 'm':
+          message = `Pleased to meet you ${userInput}`
+          break
+        case 'k':
+        case 'v':
+        case 'i':
+          message = `Glad to see you here ${userInput}`
+          break
+        case 'h':
+        case 'r':
+        case 'å':
+          message = `What a pleasent to surprise to find you here ${userInput}`
+          break
+        case 's':
+        case 'l':
+        case 'q':
+          message = `Good day ${userInput}`
+          break
+        case 'n':
+        case 'w':
+        case 'ö':
+          message = `Meeting you here ${userInput} have made this day a little bit better`
+          break
+        case 'u':
+        case 'y':
+        case 'z':
+          message = `Woooohoooooooo a wild ${userInput} appeared`
+          break
+        case 'x':
+        case 'ä':
+          message = `Hi ${userInput}, what a unique name you have`
+          break
+        default:
+          message = `That doesnt seem to be a Swedish name, check your spelling and try again.`
+      }
       const speech = new SpeechSynthesisUtterance(message)
       speechSynthesis.speak(speech)
   }
@@ -17,8 +71,8 @@ const TextToSpeech = () => {
 
   return (
     <div>
-      <input type="text" value={userInput} onChange={handleInputChange} placeholder="Enter your name"  />
-      <button onClick={handleSpeak}>Play greeting</button>
+      <input type="text" value={userInput} className={styles.inputField} onChange={handleInputChange} placeholder="Enter your name"  />
+      <button onClick={handleSpeak} className={styles.buttonStyle}>Play greeting</button>
     </div>
   )
 }
